218e47306c0ca0db4319766c1f904c4480afe3ba
Sarah <161146331+1-sarah@users.noreply.github.com>
2024-05-21 11:43:26 +0200

    Update Statistiken.py
    "" entfernt bei mama_weights.csv

--- pages/Statistiken.py
+++ pages/Statistiken.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-weight_data = github.read_df("mama_weights.csv")
+weight_data = github.read_df(mama_weights.csv)
 
 st.line_chart(data = weight_data, x=None, y=None, color=None, width=0, height=0)
