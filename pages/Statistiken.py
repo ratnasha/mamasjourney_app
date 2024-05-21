5885f81b2e4e85f452a415acffd7b185075c8a88
Sarah <161146331+1-sarah@users.noreply.github.com>
2024-05-21 19:29:18 +0200

    Update Statistiken.py
    Name und Darstellung noch verschönern

--- pages/Statistiken.py
+++ pages/Statistiken.py
@@ -27,8 +27,7 @@ def read_df(csv):
 def plot_graph(csv):
     df, xaxis, yaxis = read_df(csv)
     if df is not None and xaxis is not None and yaxis is not None:
-        st.write("### DataFrame:")
-        st.write(df)  # Display the DataFrame
+        st.write("### f{yaxis}:")
         st.write(f"X-axis: {xaxis}, Y-axis: {yaxis}")
         st.line_chart(df.set_index(xaxis)[yaxis])  # Plot the line chart
     else:
