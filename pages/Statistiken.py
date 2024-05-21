38e051dfe8b4b711ba759ef9b057c56d6eff0386
Sarah <161146331+1-sarah@users.noreply.github.com>
2024-05-21 11:45:45 +0200

    Update Statistiken.py

--- pages/Statistiken.py
+++ pages/Statistiken.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from githhub_contents import GithubContents
 
-weight_data = github.read_df(mama_weights.csv)
+# Read Dataframe for weight
+weight_data = github.read_df("mama_weights.csv")
+
 
 st.line_chart(data = weight_data, x=None, y=None, color=None, width=0, height=0)
