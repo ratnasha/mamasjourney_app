a32cedd9d72b7d2b9df33f352785bd6a023d106e
Sarah <161146331+1-sarah@users.noreply.github.com>
2024-05-21 12:49:48 +0200

    Update Statistiken.py
    Erstellen der ersten Statistik Gewicht

--- pages/Statistiken.py
+++ pages/Statistiken.py
@@ -1,9 +1,24 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Read Dataframe for weight
-weight_data = pd.read_df("mama_weights.csv")
+url_weights = 'https://github.com/ratnasha/FirstApp-data/blob/main/mama_weights.csv'
+weight_data = pd.read_csv(url_weights, parse_dates=['date'])
 
+# Darstellung der Daten
+df.set_index('date', inplace=True)
 
-st.line_chart(data = weight_data, x=None, y=None, color=None, width=0, height=0)
+def plot_line_chart():
+  plt.figure(figsize=(10, 6))
+  plt.plot(df.index, df['weight'], marker='o', linestyle='-')
+  plt.xlabel('Datum')
+  plt.ylabel('Gewicht')
+  plt.title('Gewichtsentwicklung')
+  plt.grid(True)
+  plt.xticks(rotation=45)
+  plt.tight_layout()
+  plt.show()
+
+plot_line_chart()
