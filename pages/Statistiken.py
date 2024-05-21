72f507a9a717b7417284ddf0f918448db05ba313
Sarah <161146331+1-sarah@users.noreply.github.com>
2024-05-21 13:37:24 +0200

    Update Statistiken.py

--- pages/Statistiken.py
+++ pages/Statistiken.py
@@ -1,25 +1,26 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 from github_contents import GithubContents
 
 github = GithubContents(
-            st.secrets["github"]["owner"],
-            st.secrets["github"]["repo"],
-            st.secrets["github"]["token"])
+    st.secrets["github"]["owner"],
+    st.secrets["github"]["repo"],
+    st.secrets["github"]["token"]
+)
 
 # Read Dataframe and Names for Line Graph
 def read_df(csv):
-  if github.file_exists(csv):
-            df = github.read_df(csv)
-            xaxis = pd.read_csv(csv).columns[0]
-            yaxis = pd.read_csv(csv).columns[1]
+    if github.file_exists(csv):
+        df = github.read_df(csv)
+        xaxis = pd.read_csv(csv).columns[0]
+        yaxis = pd.read_csv(csv).columns[1]
+        return df, xaxis, yaxis  # Return DataFrame and column names
 
 # Darstellung der Daten
 def plot_graph(csv):
-  data = read_df(csv)
-  '''Darstellung eines Linegraph'''
-  st.line_chart(data, x=xaxis, y=yaxis, color='#94c871', width=0, height=0)
+    df, xaxis, yaxis = read_df(csv)  # Get DataFrame and column names
+    '''Darstellung eines Linegraph'''
+    st.line_chart(df, x=xaxis, y=yaxis, color='#94c871', width=0, height=0)
 
 # Gewicht Mama1
 plot_graph('mama_weights.csv')
