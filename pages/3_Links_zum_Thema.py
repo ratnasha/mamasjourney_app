c6e68ecb860bc7ed03c07cabf13a28a5cb11d63a
Sarah <161146331+1-sarah@users.noreply.github.com>
2024-05-22 21:38:03 +0200

    Update 3_Links_zum_Thema.py

--- pages/3_Links_zum_Thema.py
+++ pages/3_Links_zum_Thema.py
@@ -3,5 +3,6 @@ import streamlit as st
 st.title("Weiterführende Links")
 
 # Adding a link button for diffrent useful websites
-st.link_button("Besuche Swissmom", "https://www.swissmom.ch")
+st.link_button("Swissmom", "https://www.swissmom.ch")
 st.link_button("Pampers Schwangerschaftskalender", "https://www.pampers.ch/de-ch/schwangerschaft/schwangerschaftskalender")
+st.link_button("Mutterschutz Schweiz","https://www.seco.admin.ch/seco/de/home/Arbeit/Arbeitsbedingungen/faq_arbeitsbedingungen/faq_mutterschutz.html")
