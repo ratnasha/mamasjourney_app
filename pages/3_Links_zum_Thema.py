2f167e744a394adc852ff70979b290d116f0c5fc
ratnasha <161899741+ratnasha@users.noreply.github.com>
2024-05-22 10:06:47 +0200

    Update 3_Links_zum_Thema.py

--- pages/3_Links_zum_Thema.py
+++ pages/3_Links_zum_Thema.py
@@ -1,1 +1,10 @@
+import streamlit as st
+
+st.title("Links")
+
+# Einfügen des Swissmom-Links
+link = '[Besuche Swissmom](https://www.swissmom.ch)'
+st.markdown(link, unsafe_allow_html=True)
+
+st.write("Dies ist eine einfache Streamlit-App mit einem Link zu Swissmom.")
 
