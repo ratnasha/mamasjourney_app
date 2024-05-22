bb0eba1626ec0a79a0e5f769edc091bc75e04e72
ratnasha <161899741+ratnasha@users.noreply.github.com>
2024-05-22 10:49:31 +0200

    Update 3_Links_zum_Thema.py

--- pages/3_Links_zum_Thema.py
+++ pages/3_Links_zum_Thema.py
@@ -7,4 +7,12 @@ link = '[Besuche Swissmom](https://www.swissmom.ch)'
 st.markdown(link, unsafe_allow_html=True)
 
 st.write("Dies ist eine einfache Streamlit-App mit einem Link zu Swissmom.")
+import streamlit as st
 
+st.title("Schwangerschaftskalender")
+
+# Einfügen des Pampers-Schwangerschaftskalender-Links
+link = '[Pampers Schwangerschaftskalender](https://www.pampers.ch/de-ch/schwangerschaft/schwangerschaftskalender)'
+st.markdown(link, unsafe_allow_html=True)
+
+st.write("Besuche den obigen Link, um den Schwangerschaftskalender von Pampers zu sehen.")
