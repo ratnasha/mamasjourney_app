de87645357ce71d82d73244e1b74c58ed71b49a3
ratnasha <161899741+ratnasha@users.noreply.github.com>
2024-05-22 08:43:40 +0200

    Update Baby.py

--- pages/Baby.py
+++ pages/Baby.py
@@ -1,39 +1,43 @@
-Baby-Timeline -Fruchtgrösse
+Liste der Fruchtgrößen
+fruchtgroessen = [
+    ("4 Wochen", "Mohnsamen"),
+    ("5 Wochen", "Sesamsamen"),
+    ("6 Wochen", "Linsenkorn"),
+    ("7 Wochen", "Blaubeere"),
+    ("8 Wochen", "Kidneybohne"),
+    ("9 Wochen", "Traube"),
+    ("10 Wochen", "Kirsche"),
+    ("11 Wochen", "Erdbeere"),
+    ("12 Wochen", "Limette"),
+    ("13 Wochen", "Erbsenschote"),
+    ("14 Wochen", "Zitrone"),
+    ("15 Wochen", "Apfel"),
+    ("16 Wochen", "Avocado"),
+    ("17 Wochen", "Rübe"),
+    ("18 Wochen", "Paprika"),
+    ("19 Wochen", "Mango"),
+    ("20 Wochen", "Banane"),
+    ("21 Wochen", "Karotte"),
+    ("22 Wochen", "Papaya"),
+    ("23 Wochen", "Grapefruit"),
+    ("24 Wochen", "Maiskolben"),
+    ("25 Wochen", "Aubergine"),
+    ("26 Wochen", "Zucchini"),
+    ("27 Wochen", "Blumenkohl"),
+    ("28 Wochen", "Aubergine"),
+    ("29 Wochen", "Butternusskürbis"),
+    ("30 Wochen", "Kohlkopf"),
+    ("31 Wochen", "Kokosnuss"),
+    ("32 Wochen", "Jicama (Yam Bean)"),
+    ("33 Wochen", "Ananas"),
+    ("34 Wochen", "Melone"),
+    ("35 Wochen", "Honigmelone"),
+    ("36 Wochen", "Romanesco"),
+    ("37 Wochen", "Lauch"),
+    ("38 Wochen", "Rhabarber"),
+    ("39 Wochen", "Wassermelone"),
+    ("40 Wochen", "Kürbis"),
+]
 
-4 Wochen: Mohnsamen
-5 Wochen: Sesamsamen
-6 Wochen: Linsenkorn
-7 Wochen: Blaubeere
-8 Wochen: Kidneybohne
-9 Wochen: Traube
-10 Wochen: Kirsche
-11 Wochen: Erdbeere
-12 Wochen: Limette
-13 Wochen: Erbsenschote
-14 Wochen: Zitrone
-15 Wochen: Apfel
-16 Wochen: Avocado
-17 Wochen: Rübe
-18 Wochen: Paprika
-19 Wochen: Mango
-20 Wochen: Banane
-21 Wochen: Karotte
-22 Wochen: Papaya
-23 Wochen: Grapefruit
-24 Wochen: Maiskolben
-25 Wochen: Aubergine
-26 Wochen: Zucchini
-27 Wochen: Blumenkohl
-28 Wochen: Aubergine
-29 Wochen: Butternusskürbis
-30 Wochen: Kohlkopf
-31 Wochen: Kokosnuss
-32 Wochen: Yam Bean
-33 Wochen: Ananas
-34 Wochen: Melone
-35 Wochen: Honigmelone
-36 Wochen: Romanesco
-37 Wochen: Lauch
-38 Wochen: Rhabarber
-39 Wochen: Wassermelone
-40 Wochen: Kürbis
+
+
