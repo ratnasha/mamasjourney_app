be09ccd2ffb1fc812a60a4495f14508f1f75cb35
Sarah <161146331+1-sarah@users.noreply.github.com>
2024-05-28 12:30:13 +0200

    Update 4_🔗_Links.py

--- pages/4_🔗_Links.py
+++ pages/4_🔗_Links.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.set_page_config(page_title="mamasjourney", page_icon=':ship:', layout="wide")
+
 st.title("Weiterführende Links :ship:")
 tab1, tab2, tab3, tab4, tab5 = st.tabs(["Allgemein","Ernährung", "Rechtsschutz", "Entwicklung", "Beschwerden"])
 
