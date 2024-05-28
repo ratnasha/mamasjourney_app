b1fcfc2e2052b4e46ec10e60741889d02c7fa9d9
Sarah <161146331+1-sarah@users.noreply.github.com>
2024-05-28 12:19:50 +0200

    Update 4_🔗_Links.py

--- pages/4_🔗_Links.py
+++ pages/4_🔗_Links.py
@@ -1,9 +1,18 @@
 import streamlit as st
 
 st.title("Weiterführende Links :ship:")
+tab1, tab2, tab3 = st.tabs(["Ernährung", "Das Recht: als Mutter", "Tipps & Tricks"])
 
-# Adding a link button for diffrent useful websites
-st.link_button("Swissmom", "https://www.swissmom.ch")
-st.link_button("Pampers Schwangerschaftskalender", "https://www.pampers.ch/de-ch/schwangerschaft/schwangerschaftskalender")
-st.link_button("Mutterschutz Schweiz","https://www.seco.admin.ch/seco/de/home/Arbeit/Arbeitsbedingungen/faq_arbeitsbedingungen/faq_mutterschutz.html")
-st.link_button("Ernährungsguide","https://www.blv.admin.ch/blv/de/home/lebensmittel-und-ernaehrung/ernaehrung/empfehlungen-informationen/lebensphasen-und-ernaehrungsformen/schwangere-und-stillende.html")
+with tab1:
+   st.header("Ernährungsberatung")
+   st.link_button("Ernährungsguide","https://www.blv.admin.ch/blv/de/home/lebensmittel-und-ernaehrung/ernaehrung/empfehlungen-informationen/lebensphasen-und-ernaehrungsformen/schwangere-und-stillende.html")
+
+with tab2:
+   st.header("Mutterschutz")
+   st.link_button("Mutterschutz Schweiz","https://www.seco.admin.ch/seco/de/home/Arbeit/Arbeitsbedingungen/faq_arbeitsbedingungen/faq_mutterschutz.html")
+
+with tab3:
+   st.header("Tipps für werdende Mütter")
+   st.link_button("Swissmom", "https://www.swissmom.ch")
+   st.link_button("Pampers Schwangerschaftskalender", "https://www.pampers.ch/de-ch/schwangerschaft/schwangerschaftskalender")
+
