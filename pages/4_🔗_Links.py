6058c5a3dd1cc7ac909d45b8412f251bb936d99b
Sarah <161146331+1-sarah@users.noreply.github.com>
2024-05-28 12:25:33 +0200

    Update 4_🔗_Links.py

--- pages/4_🔗_Links.py
+++ pages/4_🔗_Links.py
@@ -1,18 +1,25 @@
 import streamlit as st
 
 st.title("Weiterführende Links :ship:")
-tab1, tab2, tab3 = st.tabs(["Ernährung", "Das Recht: als Mutter", "Tipps & Tricks"])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Allgemein","Ernährung", "Rechtsschutz", "Entwicklung", "Beschwerden"])
 
 with tab1:
-   st.header("Ernährungsberatung")
-   st.link_button("Ernährungsguide","https://www.blv.admin.ch/blv/de/home/lebensmittel-und-ernaehrung/ernaehrung/empfehlungen-informationen/lebensphasen-und-ernaehrungsformen/schwangere-und-stillende.html")
-
+   st.header("Allgemeine Informationen")
+   st.link_button("Swissmom", "https://www.swissmom.ch")
+   
 with tab2:
+   st.header("Ernährungsberatung")
+   st.link_button("Ernährungsguide BLV","https://www.blv.admin.ch/blv/de/home/lebensmittel-und-ernaehrung/ernaehrung/empfehlungen-informationen/lebensphasen-und-ernaehrungsformen/schwangere-und-stillende.html")
+   
+with tab3:
    st.header("Mutterschutz")
    st.link_button("Mutterschutz Schweiz","https://www.seco.admin.ch/seco/de/home/Arbeit/Arbeitsbedingungen/faq_arbeitsbedingungen/faq_mutterschutz.html")
 
-with tab3:
-   st.header("Tipps für werdende Mütter")
-   st.link_button("Swissmom", "https://www.swissmom.ch")
+with tab4:
+   st.header("Entwicklung in der Schwangerschaft")
    st.link_button("Pampers Schwangerschaftskalender", "https://www.pampers.ch/de-ch/schwangerschaft/schwangerschaftskalender")
+
+with tab5:
+   st.header("Beschwerden in der Schwangerschaft")
+   st.link_button("9monate.de", "https://www.9monate.de/schwangerschaft-geburt/beschwerden-erkrankungen/")
 
