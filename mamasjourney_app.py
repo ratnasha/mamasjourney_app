edc5f89a950b8f509f3115ae84e99985d5d5b69e
Sarah <161146331+1-sarah@users.noreply.github.com>
2024-05-29 09:51:26 +0200

    Update mamasjourney_app.py

--- mamasjourney_app.py
+++ mamasjourney_app.py
@@ -85,7 +85,7 @@ def register():
 
 def about_mamasjourney():
     st.subheader("Willkommen bei deiner persönlichen Schwangerschafts-App")
-    st.markdown('Mamasjourney" ist ein digitales Schwangerschaftstagebuch für werdende Mütter. Mit dieser App können sie jederzeit auf ihre persönlichen Daten zugreifen und wichtige Informationen wie aktuelle Schwangerschaftswoche, Blutwerte und Gewicht verflogen kann. Nur die Mutter selbst hat Zugriff auf diese Daten über ihren eigenen Benutzer-Login. Sie kann ihre Informationen manuell hinzufügen. Die App erfasst die gesamten 40 Schwangerschaftswochen und bietet außerdem die Möglichkeit, Gefühle und Gedanken schriftlich festzuhalten.')
+    st.markdown('"Mamasjourney" ist ein digitales Schwangerschaftstagebuch für werdende Mütter. Mit dieser App können sie jederzeit auf ihre persönlichen Daten zugreifen und wichtige Informationen wie aktuelle Schwangerschaftswoche, Blutwerte und Gewicht verflogen kann. Nur die Mutter selbst hat Zugriff auf diese Daten über ihren eigenen Benutzer-Login. Sie kann ihre Informationen manuell hinzufügen. Die App erfasst die gesamten 40 Schwangerschaftswochen und bietet außerdem die Möglichkeit, Gefühle und Gedanken schriftlich festzuhalten.')
 
 def main(username):
     st.write(f'Willkommen *{username}*')
